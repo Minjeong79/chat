4498f9fbdec0198afc4046d37d14170037e91534
jeungsugi <yoouug72@gmail.com>
2025-03-24 17:54:42 +0900

    api관련 재 수정

--- src/app/(mainpage)/keyword/mainchate/[id]/page.tsx
+++ src/app/(mainpage)/keyword/mainchate/[id]/page.tsx
@@ -7,12 +7,13 @@ import { userContentData } from "@/app/actions/actions";
 import { DataType } from "../../../../../../lib/type";
 
 
-const getDogId = async () => {
-  const res = await fetch(`http://localhost:3000/api/postdogid?`);
-  const data = await res.json();
-  return data;
-};
-
+// const getDogId = async () => {
+//   const res = await fetch(`http://localhost:3000/api/postdogid?`,{
+//     cache: "no-store",
+//   });
+//   const data = await res.json();
+//   return data;
+// };
 
 // const getUserData = async () => {
 //   const res = await fetch(`http://localhost:3000/api/userpost?`);
@@ -24,43 +25,45 @@ export default async function ChatePage() {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  const dogId = await getDogId();
-  console.log(`${dogId.dataid} 여긴 서버 컴포넌트`);
-  const dogDb = await dogDatas(dogId.dataid);
-  const userChat = await dataSelect(dogId.dataid);
+  // const dogId = await getDogId();
+ 
+  // const dogDb = await dogDatas(dogId.dataid);
+  // const userChat = await dataSelect(dogId.dataid);
   // const userData = await getUserData();
   // console.log(`${userData.userContent} 여기 값이다!!!!!!!!!!!!!!!!!!`);
-  if (!dogId || !dogId.dataid) {
-    // dogId가 없을 때도 AiChatePage를 렌더링할 수 있도록 기본 값 설정
-    const aiData = { aianswer: "로딩중..." };
-    const userChat:DataType[] = [];
+  // if (!dogId || !dogId.dataid) {
+  //   // dogId가 없을 때도 AiChatePage를 렌더링할 수 있도록 기본 값 설정
+  //   const aiData = { aianswer: "로딩중..." };
+  //   const userChat:DataType[] = [];
 
-    return (
-      <div
-        className=""
-        style={{ width: "400px", margin: "auto", height: "600px" }}
-      >
-        <div style={{ background: 'skyblue', height: '100%', position: 'relative' }}>
-          <AiChatePage ai={aiData} userChat={userChat ?? []} dogDb={dogDb ?? []} dogId={dogId ?? 0}/>
-        </div>
-      </div>
-    );
-  }else {
-    fetch('http://localhost:3000/api/openpostai',{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',  // JSON 형식으로 전송
-      },
-      body: JSON.stringify({ dataid: dogId.dataid }),
-    }).then(response => response.json())
-    .then(data => {
-      console.log('서버 응답:', data);  // 서버의 응답 처리
+  //   return (
+  //     <div
+  //       className=""
+  //       style={{ width: "400px", margin: "auto", height: "600px" }}
+  //     >
+  //       <h1>여기로 넘어감</h1>
+  //       <div style={{ background: 'skyblue', height: '100%', position: 'relative' }}>
+  //         {/* <AiChatePage ai={aiData} userChat={userChat ?? []} dogDb={dogDb ?? []} dogId={dogId ?? 0}/> */}
+  //         <AiChatePage userChat={userChat ?? []} dogDb={dogDb ?? []} dogId={dogId ?? 0}/>
+  //       </div>
+  //     </div>
+  //   );
+  // }else {
+  //   fetch('http://localhost:3000/api/openpostai',{
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',  // JSON 형식으로 전송
+  //     },
+  //     body: JSON.stringify({ dataid: dogId.dataid }),
+  //   }).then(response => response.json())
+  //   .then(data => {
+  //     console.log('서버 응답이다아아아아아:', data);  // 서버의 응답 처리
       
-    })
-    .catch(error => {
-      console.log('서버 요청 오류:', error);  // 에러 처리
-    });
-  }
+  //   })
+  //   .catch(error => {
+  //     console.log('서버 요청 오류:', error);  // 에러 처리
+  //   });
+  // }
   
 
   // const completion = await openai.chat.completions.create({
@@ -76,29 +79,29 @@ export default async function ChatePage() {
   // });
   
   
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-     messages: [
-      { role: "assistant", content: `성별은 ${dogDb?.[0]?.gender}, 이름은 ${dogDb?.[0]?.name} 성격은 ${dogDb?.[0]?.personality} 좋아하는 것 ${dogDb?.[0]?.like} 싫어하는 것 ${dogDb?.[0]?.hate} 하는 행동은 ${dogDb?.[0]?.active}인 강아지로 대화해 사용자가 먼저 대화를 하면 대답해` },
-      {
-        role: "user",
-        content: `${userChat?.slice(-1)[0]?.content}`,
-      }, 
-    ],
-    response_format: {
-      "type": "text"
-    },
-    temperature: 1,
-    max_completion_tokens: 2048,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    store: true
-  });
+  // const response = await openai.chat.completions.create({
+  //   model: "gpt-4o-mini",
+  //    messages: [
+  //     { role: "assistant", content: `성별은 ${dogDb?.[0]?.gender}, 이름은 ${dogDb?.[0]?.name} 성격은 ${dogDb?.[0]?.personality} 좋아하는 것 ${dogDb?.[0]?.like} 싫어하는 것 ${dogDb?.[0]?.hate} 하는 행동은 ${dogDb?.[0]?.active}인 강아지로 대화해 사용자가 먼저 대화를 하면 대답해` },
+  //     {
+  //       role: "user",
+  //       content: `${userChat?.slice(-1)[0]?.content}`,
+  //     }, 
+  //   ],
+  //   response_format: {
+  //     "type": "text"
+  //   },
+  //   temperature: 1,
+  //   max_completion_tokens: 2048,
+  //   top_p: 1,
+  //   frequency_penalty: 0,
+  //   presence_penalty: 0,
+  //   store: true
+  // });
 
 
 
-  const aiData = { aianswer: response.choices[0].message.content };
+  // const aiData = { aianswer: response.choices[0].message.content };
 
 
   return (
@@ -108,7 +111,8 @@ export default async function ChatePage() {
     >
       <div style={{ background: 'skyblue', height: '100%', position: 'relative' }}>
         <div >
-          <AiChatePage ai={aiData} userChat={userChat ?? []} dogDb={dogDb ?? []}  dogId={dogId ?? 0}/>
+          {/* <AiChatePage  userChat={userChat ?? []} dogDb={dogDb ?? []}  dogId={dogId ?? 0}/> */}
+          <AiChatePage />
 
         </div>
 
--- src/app/componets/chat/aichate.tsx
+++ src/app/componets/chat/aichate.tsx
@@ -12,7 +12,8 @@ import { userContentData } from "@/app/actions/actions";
 interface AiType {
   aianswer: string | null;
 }
-export default function AiChatePage({ ai, userChat, dogId ,dogDb}: { ai: AiType; userChat: DataType[]; dogId:number; dogDb:dogDataType[]}) {
+// export default function AiChatePage({ userChat, dogId ,dogDb}: { userChat: DataType[]; dogId:number; dogDb:dogDataType[]}) {
+export default function AiChatePage() {
   const nanoid = customAlphabet("123456789", 8);
   const nid = Number(nanoid());
   // const [storeDogId, setStoreDogId] = useState<string | null>("");
@@ -21,8 +22,10 @@ export default function AiChatePage({ ai, userChat, dogId ,dogDb}: { ai: AiType;
   const useri = useStore(userUidStore, (state) => state.uid);
 
   const router = useRouter();
-  const [aian, setAian] = useState<AiType>(ai);
+  // const [aian, setAian] = useState<AiType>(ai);
   const [useriChate, setUseriChate] = useState<string>('');
+  const [userChat, setUserChat] = useState<string>('');
+  const [content, setContent] =  useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newContent = e.target.value;
@@ -47,14 +50,36 @@ export default function AiChatePage({ ai, userChat, dogId ,dogDb}: { ai: AiType;
     // router.refresh();
   }
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    // const storedData = sessionStorage.getItem('dogid');
-    // setStoreDogId(dogNumid);
-    // 로컬 스토리지에서 데이터 가져오기
-    if (dogNumid) {
+  //   // const storedData = sessionStorage.getItem('dogid');
+  //   // setStoreDogId(dogNumid);
+  //   // 로컬 스토리지에서 데이터 가져오기
+  //   if (dogNumid) {
+  //     // 데이터가 있으면, 서버로 POST 요청
+  //     fetch('/api/postdogid', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',  // JSON 형식으로 전송
+  //       },
+  //       body: JSON.stringify({ dataid: dogNumid }),  // 전송할 데이터
+  //     })
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         console.log('서버 응답:', data);  // 서버의 응답 처리
+  //         // router.refresh();
+  //       })
+  //       .catch(error => {
+  //         console.log('서버 요청 오류:', error);  // 에러 처리
+  //       });
+  //   }
+  
+   
+  // }, []);
+  useEffect(()=>{
+       if (dogNumid) {
       // 데이터가 있으면, 서버로 POST 요청
-      fetch('/api/postdogid', {
+      fetch('/api/openpostai', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',  // JSON 형식으로 전송
@@ -63,54 +88,66 @@ export default function AiChatePage({ ai, userChat, dogId ,dogDb}: { ai: AiType;
       })
         .then(response => response.json())
         .then(data => {
+          setUserChat(data.aianswer);
+          setContent(true)
           console.log('서버 응답:', data);  // 서버의 응답 처리
-          router.refresh();
         })
         .catch(error => {
           console.log('서버 요청 오류:', error);  // 에러 처리
         });
     }
-  
-   
-  }, []);
 
-  useEffect(() => {
-    const getChatData = async () => {
-      const res = await fetch(`/api/openpostai`);
-      const data = await res.json();
-      return data;
-    };
-    getChatData();
-    console.log(`${getChatData} 여긴 클라이언트`);
+    //   const talckChat = async () => {
+    //     const datas = {
+    //       id: nid,
+    //       uuid: useri,
+    //       dogid: dogNumid,
+    //       role: 'aidog',
+    //       content: aian.aianswer, // AI의 답변을 content에 저장
+    //     };
+    //     await dataInsert(datas); 
+    // };
+    // talckChat();
+  },[content])
+
+  // useEffect(() => {
+  //   const getChatData = async () => {
+  //     const res = await fetch(`/api/openpostai`);
+  //     const data = await res.json();
+  //     return data;
+  //   };
+  //   getChatData();
+  //   console.log(`${getChatData} 여긴 클라이언트`);
    
 
 
-    const talckChat = async () => {
-      if (aian.aianswer) {
-        const datas = {
-          id: nid,
-          uuid: useri,
-          dogid: dogNumid,
-          role: 'aidog',
-          content: aian.aianswer, // AI의 답변을 content에 저장
-        };
-        await dataInsert(datas); // 데이터 삽입
-      }
-    };
-    talckChat();
-    console.log(`${dogId.dataid} dogId서버컴포넌트에서 넘어 온 값`);
-    console.log(`${dogDb} dogDb서버컴포넌트에서 넘어 온 값`);
-    console.log("AI 답변 업데이트:", aian.aianswer);
+  //   const talckChat = async () => {
+  //     if (aian.aianswer) {
+  //       const datas = {
+  //         id: nid,
+  //         uuid: useri,
+  //         dogid: dogNumid,
+  //         role: 'aidog',
+  //         content: aian.aianswer, // AI의 답변을 content에 저장
+  //       };
+  //       await dataInsert(datas); // 데이터 삽입
+  //     }
+  //   };
+  //   talckChat();
    
-  }, [aian.aianswer, useriChate]);
+  //   console.log("AI 답변 업데이트:", aian.aianswer);
+   
+  // }, [aian.aianswer, useriChate]);
   
   return (<div>
-    {/* {aian.aianswer}<br /> */}
-    <ul>
+    <div style={{color:'lime'}}>{userChat}</div>
+    <br />
+    <br />
+    {/* <ul>
       {userChat.map((item, idx) => (<li key={idx}>{item.content}</li>))}
-    </ul>
-    <form onSubmit={handleSubmit} style={{ border: '2px solid lime' }}>
-      <input type="text" onChange={handleChange} placeholder="내용을 입력 해주세요" style={{ border: '1px solid red' }} />
+    </ul> */}
+    <form onSubmit={handleSubmit} style={{ border: '2px solid red' }}>
+      <input type="text" onChange={handleChange} placeholder="내용을 입력 해주세요" />
 
       <button type="submit">보내기</button>
     </form>
